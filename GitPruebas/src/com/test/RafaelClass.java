9d7580915e62c4006fb31646a77e357158baec40
mjimenez1018 <mjimenez@inidetam.edu.mx>
2020-09-03 10:01:14 -0500

    Se eliminan clases en MiguelBranch

--- GitPruebas/src/com/test/RafaelClass.java
+++
@@ -1,8 +0,0 @@
-package com.test;
-
-public class RafaelClass {
-
-	public int MetodoSuma(int a, int b) {
-		return a + b;
-	}
-}
--- GitPruebas/src/com/test/Yazzy.java
+++
@@ -1,7 +0,0 @@
-package com.test;
-
-public class Yazzy {
-	public static void main(String args []) {
-		System.out.println("USANDO GIT Y NO MORIR EN EL INTENTO");
-	}
-}
