ffb7ac7f908c5ed173766f8bb08ad4ba5607d84b
mjimenez1018 <mjimenez@inidetam.edu.mx>
2020-09-03 09:39:07 -0500

    Se eliminan clases y se modifica el main para la rama de MiguelBranch

--- GitPruebas/src/com/test/HelloWorld.java
+++ GitPruebas/src/com/test/HelloWorld.java
@@ -1,28 +1,11 @@
 package com.test;
 
 public class HelloWorld {
-	
-	private static CesarClass cesar;
-//	private static RafaelClass rafa;
-	private static Yazzy yaz;
+
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-//		cesar = new CesarClass();
-//		cesar.MetodoPrueba();
-//		
-//		MiguelClass m = new MiguelClass();
-//		System.out.println(m.toString());
-//		
-//		rafa = new RafaelClass();
-//		var res = rafa.MetodoSuma(5, 5);
-//		System.out.println("El resultado de mi suma es: " + res);
-//		
-//		yaz = new Yazzy();
-//		yaz.saludar();
-//
-//		System.out.println("HOLA MUNDO DESDE MIGUEL BRANCH");
+		System.out.println("HOLA MUNDO DESDE MIGUEL BRANCH");
 	}
 
 }
