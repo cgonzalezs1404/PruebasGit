f2120ca438cd448c31f0221f68bc37f2ec847ba5
Cesar Gonzalez <cgonzalezs@inidetam.edu.mx>
2020-09-03 10:16:35 -0500

    Se agrega clase de Cesar y se instancia un metodo en clase principal de la app.

---
+++ GitPruebas/src/com/test/CesarClass.java
@@ -0,0 +1,12 @@
+package com.test;
+
+public class CesarClass {
+	
+	public CesarClass() {}
+	
+	public void MetodoPrueba() {
+		
+		System.out.println("Metodo de prueba de Cesar");
+	}
+
+}
--- GitPruebas/src/com/test/HelloWorld.java
+++ GitPruebas/src/com/test/HelloWorld.java
@@ -1,9 +1,14 @@
 package com.test;
 
 public class HelloWorld {
+	
+	private static CesarClass cesar;
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		cesar = new CesarClass();
+		cesar.MetodoPrueba();
 
 	}
 
